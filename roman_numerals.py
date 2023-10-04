bc077688b50e68db565451e0e57922583701328b
Derek Marion <derek.j.marion@gmail.com>
2023-10-04 15:12:37 -0700

    First working to_roman function that satisfies all test case

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,40 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    output = ""
+    romanNumeralToArabic = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "XL": 40,
+        "L": 50,
+        "C": 100,
+        "CD": 400,
+        "D": 500,
+        "CM": 900,
+        "M": 1000,
+    }
+    romanNumeralPriorityOrder = [
+        "M",
+        "CM",
+        "D",
+        "CD",
+        "C",
+        "L",
+        "XL",
+        "X",
+        "IX",
+        "V",
+        "IV",
+        "I",
+    ]
+    for numeral in romanNumeralPriorityOrder:
+      quantity = num // romanNumeralToArabic[numeral] #note use of // division operator to enforce int. otherwise will return float and break logic
+      if quantity > 0:
+          i = quantity
+          while i > 0:
+              output += numeral
+              num -= romanNumeralToArabic[numeral]
+              i -= 1
+          if num == 0:
+              return output
